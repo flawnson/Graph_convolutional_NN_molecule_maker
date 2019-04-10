45a08fbe34f5b0c994bb0de2c7fb76e4688b730c
Flawnson Tong <flawnsontong1@gmail.com>
2019-04-10 08:07:52 -0400

    Update make.py

--- make.py
+++ make.py
@@ -1,9 +1,48 @@
 import os.path as osp
+import numpy as np
 import features
 
 import torch
 
 from torch_geometric.data import Data, Dataset
+
+# class MyOwnDataset(Dataset):
+#     def __init__(self, root, transform=None, pre_transform=None):
+#         super(MyOwnDataset, self).__init__(root, transform, pre_transform)
+
+#     @property
+#     def raw_file_names(self):
+#         return ["single file"]
+
+#     @property
+#     def processed_file_names(self):
+#         return ["single file"]
+
+#     def __len__(self):
+#         return len(self.processed_file_names)
+
+#     def download(self):
+#         self = self.raw_dir
+
+#     def process(self):
+#         i = 0
+#         for raw_path in self.raw_paths:
+#             # Read data from `raw_path`.
+#             for x, e_index, e_attr in list(zip(features.return_node_attr(features.data_instance),
+#                                                features.return_edge_index(features.data_instance),
+#                                                features.return_edge_attr(features.data_instance))):
+#                 data = Data(x=x, edge_index=e_index, edge_attr=e_attr)
+
+#                 if self.pre_filter is not None and not self.pre_filter(data):
+#                     continue
+
+#                 if self.pre_transform is not None:
+#                     data = self.pre_transform(data)
+
+#                 torch.save(data, osp.join(self.processed_dir, 'data_{}.pt'.format(i)))
+#                 i += 1
+
+# d = MyOwnDataset(root=r"C:\Users\Flawnson\Documents\Project Seraph & Cherub\Project Outcome\datasets")
 
 class MyOwnDataset(Dataset):
     def __init__(self, root, transform=None, pre_transform=None):
@@ -27,10 +66,8 @@ class MyOwnDataset(Dataset):
         i = 0
         for raw_path in self.raw_paths:
             # Read data from `raw_path`.
-            for x, e_index, e_attr in list(zip(features.return_node_attr(features.data_instance),
-                                               features.return_edge_index(features.data_instance),
-                                               features.return_edge_attr(features.data_instance))):
-                data = Data(x=x, edge_index=e_index, edge_attr=e_attr)
+            for node_attr, edge_index, edge_attr, target in features.return_data(features.data_instance):
+                data = Data(node_attr, edge_index, edge_attr, target)
 
                 if self.pre_filter is not None and not self.pre_filter(data):
                     continue
@@ -38,7 +75,7 @@ class MyOwnDataset(Dataset):
                 if self.pre_transform is not None:
                     data = self.pre_transform(data)
 
-                torch.save(data, osp.join(self.processed_dir, 'data_{}.pt'.format(i)))
+                torch.save(data, osp.join(self.processed_dir, 'data_{}.pth'.format(i)))
                 i += 1
 
 d = MyOwnDataset(root=r"C:\Users\Flawnson\Documents\Project Seraph & Cherub\Project Outcome\datasets")
